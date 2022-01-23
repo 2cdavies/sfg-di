9685c9f1021553a90fbb8bc2f807349ea1df17f7
Craig <crdavies@hotmail.co.uk>
2022-01-23 14:49:18 +0000

    lietuviskas

--- src/main/java/guru/springframework/sfgdi/services/i18nLithuanianService.java
+++ src/main/java/guru/springframework/sfgdi/services/i18nLithuanianService.java
@@ -4,7 +4,7 @@ package guru.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("LT")
+@Profile({"LT", "default"})
 @Service("i18nService")
 public class i18nLithuanianService implements  GreetingService {
 
